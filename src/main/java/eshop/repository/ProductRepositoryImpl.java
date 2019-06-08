9d0106426e85add5d6eb58bf927653000fa93ec4
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2019-06-08 14:37:25 +0200

    ProductRepositoryImpl: - Changed return type of update method to product - Added TODO: for Robert to check

--- src/main/java/eshop/repository/ProductRepositoryImpl.java
+++ src/main/java/eshop/repository/ProductRepositoryImpl.java
@@ -10,8 +10,10 @@ public class ProductRepositoryImpl implements ProductRepository {
 
     @Override
     public Product update(Product product) {
-        return null;
+        return product;
     }
+
+    //todo: @Robert to confirm if long required to check number of products before deletion? Why Long and not boolean?
 
     @Override
     public Long delete(Product product) {
