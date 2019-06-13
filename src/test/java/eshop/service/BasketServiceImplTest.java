93c43cbb3723fe97531a050a745d433f7596d0e6
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2019-06-13 23:20:06 +0200

    ProductValidatorTest: - Two validation methods tested:                - Product validator                - Price validator

---
+++ src/test/java/eshop/service/BasketServiceImplTest.java
@@ -0,0 +1,18 @@
+package eshop.service;
+
+import org.junit.Test;
+
+public class BasketServiceImplTest {
+
+    @Test
+    public void addProduct() {
+    }
+
+    @Test
+    public void calculateClientBasket() {
+    }
+
+    @Test
+    public void groupClientShoppingByProduct() {
+    }
+}
