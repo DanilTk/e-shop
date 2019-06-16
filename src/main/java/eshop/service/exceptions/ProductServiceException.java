c9ef1fa03ddd8a656ee6ef7cf6f76c64cfdf53bf
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2019-06-16 21:55:08 +0200

    Removed unused exception class

--- src/main/java/eshop/service/exceptions/ProductServiceException.java
+++
@@ -1,8 +0,0 @@
-package eshop.service.exceptions;
-
-public class ProductServiceException extends Exception {
-
-    public ProductServiceException(String message) {
-        super(message);
-    }
-}
