6bd347c7a3e90374b76dcdf23c71b3ef96a6bb6c
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2019-06-26 21:15:56 +0200

    Commit: importDataFromExcel method added.

--- src/main/java/eshop/service/ExcelReader.java
+++ src/main/java/eshop/service/ExcelReader.java
@@ -2,11 +2,12 @@ package eshop.service;
 
 import eshop.model.Client;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface ExcelReader {
 
-    List<Client> retrieveClients();
+    List<Client> importDataFromExcel(String excelPath) throws IOException;
 
-    List<Client> exportClients();
+    List<Client> exportDataToExcel(String savingPath);
 }
---
+++ src/test/java/eshop/service/ExcelReaderImplTest.java
@@ -0,0 +1,16 @@
+package eshop.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExcelReaderImplTest {
+
+    @Test
+    public void retrieveClients() {
+    }
+
+    @Test
+    public void exportClients() {
+    }
+}
